bd7f783da8f12dbfaa33dd6debe8fb7ec04173ae
DayriseA_dom <adrien.maisak@gmail.com>
2024-01-24 14:43:31 +0100

    pedagogy: mocking loading json files not working in our test client
    Commit to come back to later for pedagogical purposes. For now we will
    use an other way to control the data for our test environment.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,25 +2,45 @@ import pytest
 
 from server import app, clubs, competitions
 
+clubs_test_list = [
+    {"name": "Simply Test", "email": "test@simplylift.co", "points": "13"},
+    {"name": "Iron Test", "email": "test@irontemple.com", "points": "4"},
+    {"name": "She Test", "email": "test@shelifts.co.uk", "points": "12"},
+]
+
+competitions_test_list = [
+    {"name": "Spring Festival", "date": "2024-03-27 10:00:00", "numberOfPlaces": "25"},
+    {"name": "Fall Classic", "date": "2024-10-22 13:30:00", "numberOfPlaces": "13"},
+    {"name": "Winter Classic", "date": "2020-12-22 13:30:00", "numberOfPlaces": "15"},
+]
+
 
 @pytest.fixture
-def client():
+def client(mocker):
+    # Mocking loadClubs and loadCompetitions for a controlled test environment
+    # does not seem to work right now
+    mocker.patch("server.loadClubs", return_value=clubs_test_list)
+    mocker.patch("server.loadCompetitions", return_value=competitions_test_list)
+
     app.config.update({"TESTING": True})
     with app.test_client() as client:
         yield client
 
 
 @pytest.fixture
-def clubs_list():
+def clubs_list(mocker):
+    mocker.patch("server.loadClubs", return_value=clubs_test_list)
     return clubs
 
 
 @pytest.fixture
-def competitions_list():
+def competitions_list(mocker):
+    mocker.patch("server.loadCompetitions", return_value=competitions_test_list)
     return competitions
 
 
 @pytest.fixture
-def registered_mail():
+def registered_mail(mocker):
+    mocker.patch("server.loadClubs", return_value=clubs_test_list)
     email = clubs[0]["email"]
     return email
--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -2,178 +2,178 @@ import pytest
 import html
 from server import loadClubs, loadCompetitions, is_date_str_in_past
 
-REGISTERED_MAIL = "john@simplylift.co"
+
+class TestServer:
+    """Tests for the different routes in the server."""
+
+    def test_index_route(self, client):
+        response = client.get("/")
+        assert response.status_code == 200
+        assert b"Welcome to the GUDLFT Registration Portal" in response.data
+
+    def test_showSummary_with_registered_email(self, client, registered_mail):
+        response = client.post("/showSummary", data={"email": registered_mail})
+        print()
+        print(registered_mail)  # not the mocked one...
+        assert response.status_code == 200
+        assert b"Welcome" in response.data
+        assert b"Book Places" in response.data
+
+    def test_showSummary_with_unregistered_email(self, client):
+        data = {"email": "unregistered@test.com"}
+        response = client.post("/showSummary", data=data)
+        assert response.status_code == 302
+
+        # Also check that redirected to the right page
+        response = client.post("/showSummary", data=data, follow_redirects=True)
+        flash_message = html.escape("Sorry, that email was not found.")
+        assert response.status_code == 200
+        assert flash_message in response.data.decode()
+
+    def test_get_method_not_allowed_on_showSummary(self, client):
+        response = client.get("/showSummary")
+        assert response.status_code == 405
+
+    def test_book_with_valid_parameters(self, client, competitions_list, clubs_list):
+        competition = competitions_list[0]["name"]
+        club = clubs_list[0]["name"]
+        print()
+        print(clubs_list)  # not the mocked list...
+        response = client.get(f"/book/{competition}/{club}")
+        assert response.status_code == 200
+        assert b"Places available" in response.data
+        assert competition in response.data.decode()
+
+    def test_book_with_invalid_parameters(self, client):
+        response = client.get("/book/invalid_competition/invalid_club")
+        assert response.status_code == 404
+        assert b"Something went wrong" in response.data
+        assert b"Welcome" in response.data
+
+    def test_book_with_past_competition(self, client, competitions_list, clubs_list):
+        competition = competitions_list[2]["name"]
+        club = clubs_list[2]["name"]
+        response = client.get(f"/book/{competition}/{club}")
+        assert response.status_code == 200
+        assert b"past competition" in response.data
+        assert b"Welcome" in response.data
+
+    def test_get_method_not_allowed_on_purchasePlaces(self, client):
+        response = client.get("/purchasePlaces")
+        assert response.status_code == 405
+
+    def test_purchase_more_than_12_places(self, client, competitions_list, clubs_list):
+        competition = competitions_list[0]["name"]
+        club = clubs_list[0]["name"]
+        places = 13
+        data = {"competition": competition, "club": club, "places": places}
+        response = client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
+        assert b"book more than 12 places" in response.data
+        assert b"How many places" in response.data
+
+    def test_purchase_more_places_than_available(
+        self, client, competitions_list, clubs_list
+    ):
+        # define number of places available and points for the test purpose
+        competitions_list[0]["numberOfPlaces"] = 8
+        clubs_list[0]["points"] = 15
+        competition = competitions_list[0]["name"]
+        club = clubs_list[0]["name"]
+        data = {"competition": competition, "club": club, "places": 10}
+        response = client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
+        assert b"not enough places available" in response.data
+        assert b"How many places" in response.data
+
+    def test_purchase_with_not_enough_points(
+        self, client, competitions_list, clubs_list
+    ):
+        competition = competitions_list[0]["name"]
+        club = clubs_list[0]["name"]
+        # we have a ratio of 1 point per place
+        places = int(clubs_list[0]["points"]) + 1
+        # be sure that we have enough places available for the test
+        competitions_list[0]["numberOfPlaces"] = places + 1
+        data = {"competition": competition, "club": club, "places": places}
+        response = client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
+        assert b"Sorry, you don&#39;t have enough points" in response.data
+        assert b"How many places" in response.data
+
+    def test_purchase_with_places_not_an_int(
+        self, client, competitions_list, clubs_list
+    ):
+        competition = competitions_list[0]["name"]
+        club = clubs_list[0]["name"]
+        places = "two"
+        data = {"competition": competition, "club": club, "places": places}
+        with pytest.raises(ValueError):
+            client.post("/purchasePlaces", data=data)
+
+    def test_purchasePlaces_everything_ok(self, client, competitions_list, clubs_list):
+        # define number of places available and points for the test purpose
+        competitions_list[0]["numberOfPlaces"] = 20
+        clubs_list[0]["points"] = 10
+
+        competition = competitions_list[0]["name"]
+        club = clubs_list[0]["name"]
+        places = 6
+        data = {"competition": competition, "club": club, "places": places}
+        response = client.post("/purchasePlaces", data=data)
+        assert response.status_code == 200
+        assert clubs_list[0]["points"] == 4
+        assert competitions_list[0]["numberOfPlaces"] == 14
+        assert b"Great - booking complete!" in response.data
+
+    def test_logout(self, client):
+        response = client.get("/logout")
+        assert response.status_code == 302
+
+        # Also check that redirected to the right page
+        response = client.get("/logout", follow_redirects=True)
+        assert b"GUDLFT Registration" in response.data
+        assert b"Please enter your secretary email to continue" in response.data
+
+    def test_clubsPoints_display(self, client):
+        response = client.get("/clubsPoints")
+        print()
+        print(response.data.decode())  # data displayed is not the mocked one...
+        assert response.status_code == 200
+        assert b"Public list of all clubs and their current points" in response.data
 
 
-def test_loadClubs():
-    clubs = loadClubs()
-    assert isinstance(clubs, list)
-    assert len(clubs) > 0
-    for club in clubs:
-        assert "name" in club
-        assert "email" in club
-        assert "points" in club
+class TestUtilities:
+    """Unit tests for utility functions."""
 
+    def test_loadClubs(self):
+        clubs = loadClubs()
+        assert isinstance(clubs, list)
+        assert len(clubs) > 0
+        for club in clubs:
+            assert "name" in club
+            assert "email" in club
+            assert "points" in club
 
-def test_loadCompetitions():
-    competitions = loadCompetitions()
-    assert isinstance(competitions, list)
-    assert len(competitions) > 0
-    for competition in competitions:
-        assert "name" in competition
-        assert "numberOfPlaces" in competition
+    def test_loadCompetitions(self):
+        competitions = loadCompetitions()
+        assert isinstance(competitions, list)
+        assert len(competitions) > 0
+        for competition in competitions:
+            assert "name" in competition
+            assert "numberOfPlaces" in competition
 
-
-@pytest.mark.parametrize(
-    "date_str, expected",
-    [
-        ("2023-03-27 10:00:00", True),
-        ("2043-03-27 10:00:00", False),
-        ("invalid date string", pytest.raises(ValueError)),
-    ],
-)
-def test_is_date_str_in_past(date_str, expected):
-    if isinstance(expected, bool):
-        assert is_date_str_in_past(date_str) == expected
-    else:
-        with expected:
-            is_date_str_in_past(date_str)
-
-
-def test_index_route(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"Welcome to the GUDLFT Registration Portal" in response.data
-
-
-def test_showSummary_with_registered_email(client, registered_mail):
-    response = client.post("/showSummary", data={"email": registered_mail})
-    assert response.status_code == 200
-    assert b"Welcome" in response.data
-    assert b"Book Places" in response.data
-
-
-def test_showSummary_with_unregistered_email(client):
-    data = {"email": "unregistered@test.com"}
-    response = client.post("/showSummary", data=data)
-    assert response.status_code == 302
-
-    # Also check that redirected to the right page
-    response = client.post("/showSummary", data=data, follow_redirects=True)
-    flash_message = html.escape("Sorry, that email was not found.")
-    assert response.status_code == 200
-    assert flash_message in response.data.decode()
-
-
-def test_get_method_not_allowed_on_showSummary(client):
-    response = client.get("/showSummary")
-    assert response.status_code == 405
-
-
-def test_book_with_valid_parameters(client, competitions_list, clubs_list):
-    competition = competitions_list[0]["name"]
-    club = clubs_list[0]["name"]
-    response = client.get(f"/book/{competition}/{club}")
-    assert response.status_code == 200
-    assert b"Places available" in response.data
-    assert competition in response.data.decode()
-
-
-def test_book_with_invalid_parameters(client):
-    response = client.get("/book/invalid_competition/invalid_club")
-    assert response.status_code == 404
-    assert b"Something went wrong" in response.data
-    assert b"Welcome" in response.data
-
-
-def test_book_with_past_competition(client, competitions_list, clubs_list):
-    competition = competitions_list[2]["name"]
-    club = clubs_list[2]["name"]
-    response = client.get(f"/book/{competition}/{club}")
-    assert response.status_code == 200
-    assert b"past competition" in response.data
-    assert b"Welcome" in response.data
-
-
-def test_get_method_not_allowed_on_purchasePlaces(client):
-    response = client.get("/purchasePlaces")
-    assert response.status_code == 405
-
-
-def test_purchase_more_than_12_places(client, competitions_list, clubs_list):
-    competition = competitions_list[0]["name"]
-    club = clubs_list[0]["name"]
-    places = 13
-    data = {"competition": competition, "club": club, "places": places}
-    response = client.post("/purchasePlaces", data=data)
-    assert response.status_code == 200
-    assert b"book more than 12 places" in response.data
-    assert b"How many places" in response.data
-
-
-def test_purchase_more_places_than_available(client, competitions_list, clubs_list):
-    # define number of places available and points for the test purpose
-    competitions_list[0]["numberOfPlaces"] = 8
-    clubs_list[0]["points"] = 15
-    competition = competitions_list[0]["name"]
-    club = clubs_list[0]["name"]
-    data = {"competition": competition, "club": club, "places": 10}
-    response = client.post("/purchasePlaces", data=data)
-    assert response.status_code == 200
-    assert b"not enough places available" in response.data
-    assert b"How many places" in response.data
-
-
-def test_purchase_with_not_enough_points(client, competitions_list, clubs_list):
-    competition = competitions_list[0]["name"]
-    club = clubs_list[0]["name"]
-    # we have a ratio of 1 point per place
-    places = int(clubs_list[0]["points"]) + 1
-    # be sure that we have enough places available for the test
-    competitions_list[0]["numberOfPlaces"] = places + 1
-    data = {"competition": competition, "club": club, "places": places}
-    response = client.post("/purchasePlaces", data=data)
-    assert response.status_code == 200
-    assert b"Sorry, you don&#39;t have enough points" in response.data
-    assert b"How many places" in response.data
-
-
-def test_purchase_with_places_not_an_int(client, competitions_list, clubs_list):
-    competition = competitions_list[0]["name"]
-    club = clubs_list[0]["name"]
-    places = "two"
-    data = {"competition": competition, "club": club, "places": places}
-    with pytest.raises(ValueError):
-        client.post("/purchasePlaces", data=data)
-
-
-def test_purchasePlaces_everything_ok(client, competitions_list, clubs_list):
-    # define number of places available and points for the test purpose
-    competitions_list[0]["numberOfPlaces"] = 20
-    clubs_list[0]["points"] = 10
-
-    competition = competitions_list[0]["name"]
-    club = clubs_list[0]["name"]
-    places = 6
-    data = {"competition": competition, "club": club, "places": places}
-    response = client.post("/purchasePlaces", data=data)
-    assert response.status_code == 200
-    assert clubs_list[0]["points"] == 4
-    assert competitions_list[0]["numberOfPlaces"] == 14
-    assert b"Great - booking complete!" in response.data
-
-
-def test_logout(client):
-    response = client.get("/logout")
-    assert response.status_code == 302
-
-    # Also check that redirected to the right page
-    response = client.get("/logout", follow_redirects=True)
-    assert b"GUDLFT Registration" in response.data
-    assert b"Please enter your secretary email to continue" in response.data
-
-
-def test_clubsPoints_display(client, clubs_list):
-    response = client.get("/clubsPoints")
-    assert response.status_code == 200
-    assert b"Public list of all clubs and their current points" in response.data
+    @pytest.mark.parametrize(
+        "date_str, expected",
+        [
+            ("2023-03-27 10:00:00", True),
+            ("2043-03-27 10:00:00", False),
+            ("invalid date string", pytest.raises(ValueError)),
+        ],
+    )
+    def test_is_date_str_in_past(self, date_str, expected):
+        if isinstance(expected, bool):
+            assert is_date_str_in_past(date_str) == expected
+        else:
+            with expected:
+                is_date_str_in_past(date_str)
