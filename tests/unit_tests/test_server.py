cbd3f133b72c1288392f0b5e162f47dd0f32817a
DayriseA_dom <adrien.maisak@gmail.com>
2024-01-24 20:41:05 +0100

    refactor: refactor unit tests and use predeterminated data
    - tests regrouped in classes
    - use predeterminated data for tests by changing the clubs and
      competitions global variables from the server module

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -1,20 +1,51 @@
 import pytest
 import html
-from server import loadClubs, loadCompetitions, is_date_str_in_past
+import server
 
 
 class TestServer:
     """Tests for the different routes in the server."""
+
+    @classmethod
+    def setup_class(cls):
+        """Setup the server for testing with predetermined data."""
+        server.clubs.clear()
+        server.clubs.extend(
+            [
+                {"name": "Simply Test", "email": "test@simplylift.co", "points": "13"},
+                {"name": "Iron Test", "email": "test@irontemple.com", "points": "4"},
+                {"name": "She Test", "email": "test@shelifts.co.uk", "points": "12"},
+            ]
+        )
+        server.competitions.clear()
+        server.competitions.extend(
+            [
+                {
+                    "name": "Spring Festival",
+                    "date": "2024-03-27 10:00:00",
+                    "numberOfPlaces": "25",
+                },
+                {
+                    "name": "Fall Classic",
+                    "date": "2024-10-22 13:30:00",
+                    "numberOfPlaces": "5",
+                },
+                {
+                    "name": "Winter Past Date",
+                    "date": "2020-12-22 13:30:00",
+                    "numberOfPlaces": "15",
+                },
+            ]
+        )
 
     def test_index_route(self, client):
         response = client.get("/")
         assert response.status_code == 200
         assert b"Welcome to the GUDLFT Registration Portal" in response.data
 
-    def test_showSummary_with_registered_email(self, client, registered_mail):
+    def test_showSummary_with_registered_email(self, client):
+        registered_mail = server.clubs[0]["email"]
         response = client.post("/showSummary", data={"email": registered_mail})
-        print()
-        print(registered_mail)  # not the mocked one...
         assert response.status_code == 200
         assert b"Welcome" in response.data
         assert b"Book Places" in response.data
@@ -34,11 +65,9 @@ class TestServer:
         response = client.get("/showSummary")
         assert response.status_code == 405
 
-    def test_book_with_valid_parameters(self, client, competitions_list, clubs_list):
-        competition = competitions_list[0]["name"]
-        club = clubs_list[0]["name"]
-        print()
-        print(clubs_list)  # not the mocked list...
+    def test_book_with_valid_parameters(self, client):
+        competition = server.competitions[0]["name"]
+        club = server.clubs[0]["name"]
         response = client.get(f"/book/{competition}/{club}")
         assert response.status_code == 200
         assert b"Places available" in response.data
@@ -50,9 +79,9 @@ class TestServer:
         assert b"Something went wrong" in response.data
         assert b"Welcome" in response.data
 
-    def test_book_with_past_competition(self, client, competitions_list, clubs_list):
-        competition = competitions_list[2]["name"]
-        club = clubs_list[2]["name"]
+    def test_book_with_past_competition(self, client):
+        competition = server.competitions[2]["name"]
+        club = server.clubs[0]["name"]
         response = client.get(f"/book/{competition}/{club}")
         assert response.status_code == 200
         assert b"past competition" in response.data
@@ -62,9 +91,9 @@ class TestServer:
         response = client.get("/purchasePlaces")
         assert response.status_code == 405
 
-    def test_purchase_more_than_12_places(self, client, competitions_list, clubs_list):
-        competition = competitions_list[0]["name"]
-        club = clubs_list[0]["name"]
+    def test_purchase_more_than_12_places(self, client):
+        competition = server.competitions[0]["name"]
+        club = server.clubs[0]["name"]
         places = 13
         data = {"competition": competition, "club": club, "places": places}
         response = client.post("/purchasePlaces", data=data)
@@ -72,58 +101,47 @@ class TestServer:
         assert b"book more than 12 places" in response.data
         assert b"How many places" in response.data
 
-    def test_purchase_more_places_than_available(
-        self, client, competitions_list, clubs_list
-    ):
-        # define number of places available and points for the test purpose
-        competitions_list[0]["numberOfPlaces"] = 8
-        clubs_list[0]["points"] = 15
-        competition = competitions_list[0]["name"]
-        club = clubs_list[0]["name"]
+    def test_purchase_more_places_than_available(self, client):
+        competition = server.competitions[1]["name"]
+        club = server.clubs[0]["name"]
         data = {"competition": competition, "club": club, "places": 10}
         response = client.post("/purchasePlaces", data=data)
         assert response.status_code == 200
         assert b"not enough places available" in response.data
         assert b"How many places" in response.data
 
-    def test_purchase_with_not_enough_points(
-        self, client, competitions_list, clubs_list
-    ):
-        competition = competitions_list[0]["name"]
-        club = clubs_list[0]["name"]
+    def test_purchase_with_not_enough_points(self, client):
+        competition = server.competitions[0]["name"]
+        club = server.clubs[1]["name"]
         # we have a ratio of 1 point per place
-        places = int(clubs_list[0]["points"]) + 1
+        places = int(server.clubs[1]["points"]) + 1
         # be sure that we have enough places available for the test
-        competitions_list[0]["numberOfPlaces"] = places + 1
+        server.competitions[0]["numberOfPlaces"] = places + 1
         data = {"competition": competition, "club": club, "places": places}
         response = client.post("/purchasePlaces", data=data)
         assert response.status_code == 200
         assert b"Sorry, you don&#39;t have enough points" in response.data
         assert b"How many places" in response.data
 
-    def test_purchase_with_places_not_an_int(
-        self, client, competitions_list, clubs_list
-    ):
-        competition = competitions_list[0]["name"]
-        club = clubs_list[0]["name"]
+    def test_purchase_with_places_not_an_int(self, client):
+        competition = server.competitions[0]["name"]
+        club = server.clubs[0]["name"]
         places = "two"
         data = {"competition": competition, "club": club, "places": places}
         with pytest.raises(ValueError):
             client.post("/purchasePlaces", data=data)
 
-    def test_purchasePlaces_everything_ok(self, client, competitions_list, clubs_list):
-        # define number of places available and points for the test purpose
-        competitions_list[0]["numberOfPlaces"] = 20
-        clubs_list[0]["points"] = 10
-
-        competition = competitions_list[0]["name"]
-        club = clubs_list[0]["name"]
+    def test_purchasePlaces_everything_ok(self, client):
+        competition = server.competitions[0]["name"]
+        club = server.clubs[0]["name"]
         places = 6
+        expected_points = int(server.clubs[0]["points"]) - places
+        expected_places = int(server.competitions[0]["numberOfPlaces"]) - places
         data = {"competition": competition, "club": club, "places": places}
         response = client.post("/purchasePlaces", data=data)
         assert response.status_code == 200
-        assert clubs_list[0]["points"] == 4
-        assert competitions_list[0]["numberOfPlaces"] == 14
+        assert int(server.clubs[0]["points"]) == expected_points
+        assert int(server.competitions[0]["numberOfPlaces"]) == expected_places
         assert b"Great - booking complete!" in response.data
 
     def test_logout(self, client):
@@ -137,8 +155,6 @@ class TestServer:
 
     def test_clubsPoints_display(self, client):
         response = client.get("/clubsPoints")
-        print()
-        print(response.data.decode())  # data displayed is not the mocked one...
         assert response.status_code == 200
         assert b"Public list of all clubs and their current points" in response.data
 
@@ -147,7 +163,7 @@ class TestUtilities:
     """Unit tests for utility functions."""
 
     def test_loadClubs(self):
-        clubs = loadClubs()
+        clubs = server.loadClubs()
         assert isinstance(clubs, list)
         assert len(clubs) > 0
         for club in clubs:
@@ -156,7 +172,7 @@ class TestUtilities:
             assert "points" in club
 
     def test_loadCompetitions(self):
-        competitions = loadCompetitions()
+        competitions = server.loadCompetitions()
         assert isinstance(competitions, list)
         assert len(competitions) > 0
         for competition in competitions:
@@ -173,7 +189,7 @@ class TestUtilities:
     )
     def test_is_date_str_in_past(self, date_str, expected):
         if isinstance(expected, bool):
-            assert is_date_str_in_past(date_str) == expected
+            assert server.is_date_str_in_past(date_str) == expected
         else:
             with expected:
-                is_date_str_in_past(date_str)
+                server.is_date_str_in_past(date_str)
